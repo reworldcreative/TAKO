650ae7358cbe67ebc9be6bb318e01b066380a5eb
Pavlo Prokopchuk <reworldcreative@gmail.com>
2025-07-04 15:20:45 +0300

    lazy loading video

--- src/js/pages/home/media-about-us.js
+++ src/js/pages/home/media-about-us.js
@@ -5,4 +5,28 @@ export function mediaAboutUsSlider() {
     slidesPerView: 1,
     spaceBetween: 5,
   });
+
+  const wrappers = document.querySelectorAll('.media-about-us__video');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const wrapper = entry.target;
+        const videoId = wrapper.dataset.videoId;
+
+        const iframe = document.createElement('iframe');
+        iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`;
+        iframe.width = '771';
+        iframe.height = '392';
+        iframe.frameBorder = '0';
+        iframe.allow = 'autoplay; encrypted-media';
+        iframe.allowFullscreen = true;
+
+        wrapper.appendChild(iframe);
+        observer.unobserve(wrapper);
+      }
+    });
+  }, { threshold: 0.25 });
+
+  wrappers.forEach(wrapper => observer.observe(wrapper));
 }
