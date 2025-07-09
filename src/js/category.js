b657fb14415a651a2e0bc0598a80f751e04e8d2d
Pavlo Prokopchuk <reworldcreative@gmail.com>
2025-07-09 09:25:37 +0300

    optimization and category page categories section

--- src/js/category.js
+++ src/js/category.js
@@ -4,6 +4,7 @@ import { globalSettings } from "@/js/utils/global";
 import { copyBankDetails } from "@/js/pages/home/bank-details";
 import { otherCategoriesSlider } from "./pages/category/other-categories";
 import { printTextAnimation } from "./components/print-text-animation";
+import { appearanceAnimation } from "./components/appearance-animation";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { observeElements } from "./components/observe-once";
 
@@ -11,8 +12,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const observeConfigs = [
     {
       selector: ".hero-banner",
-      callback: () => printTextAnimation(".hero-banner__title", ".hero-banner")
+      callback: () => {
+        printTextAnimation(".hero-banner__title", ".hero-banner")
+        appearanceAnimation([".hero-banner__text"], ".hero-banner")
+      }
     },
+    {
+      selector: ".about",
+      callback: () => appearanceAnimation([".about__title", ".about__text"], ".about__container")
+    }
   ];
 
   ScrollTrigger.killAll();
